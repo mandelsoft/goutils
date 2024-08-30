afe3bd21b66590939bcd8a45ed0e34da6b6b71db
Uwe Krueger <uwe.krueger@sap.com>
2024-08-30 22:06:01 +0200

    optional generic option

--- optionutils/genericopt.go
+++ optionutils/genericopt.go
@@ -15,7 +15,7 @@ func (o *genricoption[S, B, T]) ApplyTo(opts B) {
 	reflectutils.CallMethodByInterfaceVA[S](opts, o.value)
 }
 
-// WithGenericOption probides a generic option implementation for Option[T]
+// WithGenericOption provides a generic option implementation for Option[T]
 // intended for options based on an option setter interface S implemented
 // by the option set B implementing S for the value type T. Hereby, B must
 // implement S, which cannot be expressed by Go generics.
@@ -26,4 +26,13 @@ func WithGenericOption[S, B any, T any](v T) Option[B] {
 		panic(fmt.Sprintf("%T must be %s", b, generics.TypeOf[S]()))
 	}
 	return &genricoption[S, B, T]{v}
+}
+
+// WithOptionalGenericOption povides an option if the given pointer
+// is not nil. It the behaves like WithGenericOption for the de-referenced value.
+func WithOptionalGenericOption[S, B any, T any](v *T) Option[B] {
+	if v != nil {
+		return WithGenericOption[S, B, T](*v)
+	}
+	return NoOption[B]{}
 }
