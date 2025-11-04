a79e705042aac1dba58b4982f7175be82ed82a12
Uwe Krueger <uwe.krueger@beauty-of-chaos.de>
2025-11-04 16:38:34 +0100

    mapped iterator

--- iterutils/for.go
+++ iterutils/for.go
@@ -1,11 +1,27 @@
 package iterutils
 
-import "iter"
+import (
+	"github.com/mandelsoft/goutils/general"
+	"iter"
+)
 
+// For provides an iterator for a sequence of elements.
 func For[T any](v ...T) iter.Seq[T] {
 	return func(yield func(v T) bool) {
 		for _, e := range v {
 			if !yield(e) {
+				return
+			}
+		}
+	}
+}
+
+// ForMapped provides an iterator providing elements of type O for a given
+// sequence of elements of type T mapped to a target type O.
+func ForMapped[O, T any](mapper general.MapperFunc[T, O], v ...T) iter.Seq[O] {
+	return func(yield func(v O) bool) {
+		for _, e := range v {
+			if !yield(mapper(e)) {
 				return
 			}
 		}
--- iterutils/iterutils.go
+++ iterutils/iterutils.go
@@ -1,13 +1,26 @@
 package iterutils
 
-import "iter"
+import (
+	"github.com/mandelsoft/goutils/general"
+	"iter"
+)
 
-// Convert transforms an iterator of type I into an iterator of type O by applying a type assertion during iteration.
+// Convert transforms an iterator of type I into an iterator of type O by
+// applying a type assertion during iteration.
 func Convert[O, I any](in iter.Seq[I]) iter.Seq[O] {
 	return func(yield func(O) bool) {
 		in(func(v I) bool {
-			var o any = v
-			return yield(o.(O))
+			return yield(any(v).(O))
+		})
+	}
+}
+
+// ConvertFunc transforms an input iterator using a provided mapper function
+// and returns a new iterator of mapped outputs.
+func ConvertFunc[O, I any](in iter.Seq[I], mapper general.MapperFunc[I, O]) iter.Seq[O] {
+	return func(yield func(O) bool) {
+		in(func(v I) bool {
+			return yield(mapper(v))
 		})
 	}
 }
